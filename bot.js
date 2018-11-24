aa2f75f4e8d56dbc8b0cb9b9d32fccb7b121ca6c
danino123 <30401731+danino123@users.noreply.github.com>
2018-11-24 14:47:08 +0200

    bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,25 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.token;
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+bot.on('message', (message) => {
+    if(message.content == 'bot-info') {
+        message.channel.senMessage ('Този бот е създаден от danino123! Версия на бота:V0.1 alpha');
+    }
 });
+bot.on('message'), (message) => {
+    if(message.content = '!help') {
+      message.channel.senMessage('SOON');
+    }
+};
 
+bot.on('message'), (message) => {
+    if(message.content = '!creator') {
+      message.channel.senMessage('Този бот е направен от danino123#5250');
+    }
+};
 bot.login('NTAxMDI4NTg5MTAyNDMyMjg3.DtquIA.T7Hwnz5lNlTUZsCJz7klyvhIP_0');
 
 
